97ddf83f816d92ff7a8111a4998bfb01ff07f994
lucky-ti <88631976+lucky-ti@users.noreply.github.com>
2024-10-11 14:40:01 +0800

    Update insertionsort.sol

--- task2/src/insertionsort.sol
+++ task2/src/insertionsort.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+contract insertionsort{
+    function sort(uint[] memory a) public pure returns(uint[] memory){
+        require (a.length > 1 , "not need to sort");
+        for(uint i = 1 ; i < a.length ; i++) {
+            uint key = a[i];
+            uint j = i;
+            while ((j >= 1) && (key <= a[j-1])){
+                a[j] = a[j-1];
+                j--;
+            }
+            a[j] = key;
+        }
+        return a;
+    }
+}
 
